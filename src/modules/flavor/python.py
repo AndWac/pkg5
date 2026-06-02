e10a901e4a9223ea73aca9001f738cd8ee6c7dc7
Jakub Kulik <jakub.kulik@oracle.com>
2026-06-02 05:39:43 -0700

    39287726 python flavor should use safer ast.literal_eval rather than eval
    (cherry picked from commit a953517d12c8a6c718f038d7a9a114b5f67c862d)

--- src/modules/flavor/python.py
+++ src/modules/flavor/python.py
@@ -21,9 +21,10 @@
 #
 
 #
-# Copyright (c) 2009, 2019, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2009, 2026, Oracle and/or its affiliates. All rights reserved.
 #
 
+import ast
 import os
 import re
 import subprocess
@@ -302,7 +303,7 @@ def process_python_dependencies(action, pkg_vars, script_path, run_paths):
                 l = l.strip()
                 if l.startswith("DEP "):
                         try:
-                                names, dirs = eval(l[4:])
+                                names, dirs = ast.literal_eval(l[4:])
                         except Exception:
                                 bad_lines.append(l)
                         else:
